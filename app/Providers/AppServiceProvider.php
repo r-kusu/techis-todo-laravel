4e6bcd49eb287c53e9adfa43d5d2efc8d5b360e9
r-kusu <ryumaexile@gmail.com>
2021-10-14 21:29:48 +0900

    change https2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         //
         if (\App::environment(['production'])) {
-            \URL::forseScheme('https');
+            \URL::forceScheme('https');
         }
     }
 }
